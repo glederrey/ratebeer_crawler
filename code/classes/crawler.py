e35936205372da9d339f9b9cb174a0223138672b
glederrey <gael.lederrey@epfl.ch>
2017-07-24 18:09:46 +0200

    [UPD] Beginning of crawling + parsing. But there's a problem with the encoding

--- code/classes/crawler.py
+++ code/classes/crawler.py
@@ -5,3 +5,126 @@
 #
 # Distributed under terms of the MIT license.
 
+import pandas as pd
+import numpy as np
+import requests
+import time
+import re
+import os
+
+
+class Crawler:
+    """
+    Crawler for RateBeer website
+    """
+
+    def __init__(self, delta_t, data_folder=None):
+        """
+        Initialize the class.
+
+        :param delta_t: Average time in seconds between two requests
+        :param data_folder: Folder to save the data
+        """
+
+        if data_folder is None:
+            self.data_folder = '../data/'
+        else:
+            self.data_folder = data_folder
+
+        self.delta_t = delta_t
+
+        self.special_places = ['United States', 'Canada', 'England', 'Germany']
+        self.codes = {213: 'United States', 39: 'Canada', 240: 'England', 79: 'Germany'}
+
+    ########################################################################################
+    ##                                                                                    ##
+    ##                               Crawl the places                                     ##
+    ##                                                                                    ##
+    ########################################################################################
+
+    def crawl_all_places(self):
+        """
+        STEP 1
+
+        Crawl all the places
+        """
+
+        url_places = 'https://www.ratebeer.com/breweries/'
+
+        # Create folder for all the HTML pages
+        folder = self.data_folder + 'misc/'
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        # Crawl the countries
+        r = self.request_and_wait(url_places)
+        with open(folder + 'places.html', 'wb') as output:
+            output.write(r.content)
+
+        # Create folder for all the places
+        folder = self.data_folder + 'places/'
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        # Open the HTML file and parse it to get all the links
+        html = open(self.data_folder + 'misc/places.html', 'rb').read().decode('ISO-8859-1')
+
+        str_ = 'href="/breweries/(\S+)/(\d+)/(\d+)/">(.+?)</a>'
+
+        grp = re.finditer(str_, str(html))
+
+        # Go through all the links and download them
+        for g in grp:
+            place_small = g.group(1)
+            region_code = int(g.group(2))
+            country_code = int(g.group(3))
+            place = g.group(4)
+
+            if 'Palestine' in place:
+                place = 'Palestine'
+
+            folder = self.data_folder + 'places/'
+
+            if country_code in self.codes.keys():
+                folder += self.codes[country_code] + '/'
+
+            folder += place + '/'
+
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+
+            url = 'https://www.ratebeer.com/breweries/{}/{:d}/{:d}/'.format(place_small, region_code, country_code)
+            r = self.request_and_wait(url)
+            with open(folder + 'brew.html', 'wb') as output:
+                output.write(r.content)
+
+    ########################################################################################
+    ##                                                                                    ##
+    ##                                Other functions                                     ##
+    ##                                                                                    ##
+    ########################################################################################
+
+    def request_and_wait(self, url):
+        """
+        Run the function get from the package requests, then wait a certain amount of time.
+
+        :param url: url for the requests
+        :return r: the request
+        """
+
+        # Get the time we want to wait before running the function again
+        delta = np.abs(np.random.normal(self.delta_t, self.delta_t/2))
+
+        start = time.time()
+
+        # Run the function
+        r = requests.get(url)
+
+        elapsed = time.time()-start
+
+        # If not enough time has been spend, sleep
+        if elapsed < delta:
+            time.sleep(delta-elapsed)
+
+        # Return the result
+        return r
--- code/classes/parser.py
+++ code/classes/parser.py
@@ -5,3 +5,105 @@
 #
 # Distributed under terms of the MIT license.
 
+import multiprocessing as mp
+import pandas as pd
+import re
+import os
+
+
+class Parser:
+    """
+    Parser for BeerAdvocate website
+    """
+
+    def __init__(self, nbr_threads=None, data_folder=None):
+        """
+        Initialize the class
+
+        :param nbr_threads: Number of threads you want to give. If not given, then it will use all the possible ones.
+        :param data_folder: Folder to save the data
+        """
+
+        if data_folder is None:
+            self.data_folder = '../data/'
+        else:
+            self.data_folder = data_folder
+
+        if nbr_threads is None:
+            self.threads = mp.cpu_count()
+        else:
+            self.threads = nbr_threads
+
+        self.special_places = ['United States', 'Canada', 'England', 'Germany']
+        self.codes = {213: 'United_States', 39: 'Canada', 240: 'England', 79: 'Germany'}
+
+    ########################################################################################
+    ##                                                                                    ##
+    ##                       Parse the breweries from the places                          ##
+    ##                                                                                    ##
+    ########################################################################################
+
+    def parse_breweries_from_places(self):
+        """
+        STEP 2
+
+        Parse all the breweries name and ID from the places
+        """
+
+        folder = self.data_folder + 'parsed/'
+        # Create folder for the parsed CSV tables
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        folder = self.data_folder + 'places/'
+
+        list_ = os.listdir(folder)
+
+        json_brewery = {'name': [], 'id': [], 'place': [], 'link': []}
+        # Go through all the countries
+        for country in list_:
+            # Check if the country is in the list of special countries
+            if country not in self.special_places:
+                # Open the file...
+                html = open(folder + country + '/brew.html', 'rb').read().decode('ISO-8859-1')
+
+                # ... and parse it
+                str_ = '<A HREF="/brewers/(\S+)/(\d+)/"> (.+?)</A>'
+
+                grp = re.finditer(str_, str(html))
+                for g in grp:
+                    link = 'https://www.ratebeer.com/brewers/{}/{}/'.format(g.group(1), g.group(2))
+
+                    json_brewery['name'].append(g.group(3))
+                    json_brewery['id'].append(g.group(2))
+                    json_brewery['link'].append(link)
+                    json_brewery['place'].append(country)
+
+            else:
+                # Get the list of regions
+                list_2 = os.listdir(folder + country)
+                # Go through all regions
+                for region in list_2:
+                    # Open the file...
+                    html = open(folder + country + '/' + region + '/brew.html', 'rb').read().decode('ISO-8859-1')
+
+                    # ... and parse it
+                    str_ = '<A HREF="/brewers/(\S+)/(\d+)/"> (.+?)</A>'
+
+                    grp = re.finditer(str_, str(html))
+                    for g in grp:
+                        link = 'https://www.ratebeer.com/brewers/{}/{}/'.format(g.group(1), g.group(2))
+
+                        json_brewery['name'].append(g.group(3))
+                        json_brewery['id'].append(g.group(2))
+                        json_brewery['link'].append(link)
+                        json_brewery['place'].append(country)
+
+        # Transform into pandas DF
+        df = pd.DataFrame(json_brewery)
+
+        # Save it
+        df.to_csv(self.data_folder + 'parsed/breweries.csv', index=False)
+
+
+
