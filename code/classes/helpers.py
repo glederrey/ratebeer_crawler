bb51d21ceb4599113666b09615355e93a2847b86
glederrey <gael.lederrey@epfl.ch>
2017-07-28 15:44:52 +0200

    [ADD] function parse in helpers

--- code/classes/helpers.py
+++ code/classes/helpers.py
@@ -16,3 +16,32 @@ def round_(x, base=50):
     :return: Rounded number
     """
     return int(base * np.floor(float(x)/base))
+
+
+def parse(filename):
+    """
+    Parse a txt.gz file and return a generator for it
+
+    Copyright © 2017 Gael Lederrey <gael.lederrey@epfl.ch>
+
+    :param filename: name of the file
+    :return: Generator to go through the file
+    """
+    file = gzip.open(filename, 'rb')
+    entry = {}
+    # Go through all the lines
+    for line in file:
+        # Transform the string-bytes into a string
+        line = line.decode("utf-8").strip()
+
+        # We check for a colon in each line
+        colon_pos = line.find(":")
+        if colon_pos == -1:
+            # if no, we yield the entry
+            yield entry
+            entry = {}
+            continue
+        # otherwise, we add the key-value pair to the entry
+        key = line[:colon_pos]
+        value = line[colon_pos + 2:]
+        entry[key] = value
