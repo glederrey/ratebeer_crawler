a669b6c49f0b75e439a3887c19ae8d84d3d189f5
glederrey <gael.lederrey@epfl.ch>
2017-08-07 13:12:23 +0200

    [UPD] run.py

--- code/run_rb.py
+++ code/run_rb.py
@@ -29,31 +29,31 @@ def run():
     parser = Parser(data_folder)
 
     print('1. Crawling all the places...')
-    crawler.crawl_all_places()
+    #crawler.crawl_all_places()
 
     print('2. Parsing the breweries from the places...')
-    parser.parse_breweries_from_places()
+    #parser.parse_breweries_from_places()
 
     print('3. Crawling all the breweries...')
-    crawler.crawl_all_breweries()
+    #crawler.crawl_all_breweries()
 
     print('4. Parsing the breweries to get the beers...')
-    parser.parse_brewery_files()
+    #parser.parse_brewery_files()
 
     print('5. Crawling all the beers and their reviews...')
-    crawler.crawl_all_beers_and_reviews()
+    #crawler.crawl_all_beers_and_reviews()
 
     print('6. Parsing all the beer files to update the beers.csv file...')
-    parser.parse_beer_files_for_information()
+    #parser.parse_beer_files_for_information()
 
     print('7. Parsing all the beer files to get the reviews...')
-    parser.parse_beer_files_for_reviews()
+    #parser.parse_beer_files_for_reviews()
 
     print('8. Getting the users from the ratings...')
-    parser.get_users_from_ratings()
+    #parser.get_users_from_ratings()
 
     print('9. Crawling all the users...')
-    crawler.crawl_all_users()
+    #crawler.crawl_all_users()
 
     print('10. Parsing the users for some information...')
     parser.parse_all_users()
